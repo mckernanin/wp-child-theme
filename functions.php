a8ab29d1a5375389aedfab82a390cb6b0da9ba37
Kevin McKernan <kevin@mckernan.in>
2016-04-27 11:31:15 -0600

    convert functions.php to class, remove unnecessary gulp tasks, update npm dependencies

--- functions.php
+++ functions.php
@@ -1,26 +1,69 @@
 <?php
-function cc_mime_types( $mimes ) {
-	$mimes['svg'] = 'image/svg+xml';
 
-	return $mimes;
-}
-add_filter( 'upload_mimes', 'cc_mime_types' );
+class ChildTheme {
 
-/**
- * TypeKit Fonts.
- *
- * @since Theme 1.0
- */
-function theme_typekit() {
-	wp_enqueue_script( 'theme_typekit', '//use.typekit.net/ihq0jhu.js' );
-}
-add_action( 'wp_enqueue_scripts', 'theme_typekit' );
+	function __construct() {
 
-function theme_typekit_inline(){
-	if ( wp_script_is( 'theme_typekit', 'done' ) ) {
-		?>
-	  <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
-<?php
+		add_filter( 'upload_mimes', 		array( $this, 'mime_types' ) );
+		add_filter( 'body_class',   		array( $this, 'add_slug_body_class' ) );
+		add_action( 'send_headers', 		array( $this, 'custom_headers' ) );
+		add_action( 'wp_enqueue_scripts', 	array( $this, 'typekit' ) );
+
+		$this->roots_support();
+	}
+
+	/**
+	 * Add custom allowed media upload file types.
+	 */
+
+	public function mime_types( $mimes ) {
+		$mimes['svg'] = 'image/svg+xml';
+
+		return $mimes;
+	}
+
+	/**
+	 * Add page slug as a body class.
+	 */
+
+	public function add_slug_body_class( $classes ) {
+		global $post;
+		if ( isset( $post ) ) {
+			$classes[] = $post->post_type . '-' . $post->post_name;
+		}
+		return $classes;
+	}
+
+	/**
+	 * Add custom headers.
+	 */
+	public function custom_headers() {
+		header( 'Access-Control-Allow-Origin: *' );
+	}
+
+	/**
+	 * Add theme support for Soil functions. Helps clean up WordPress markup.
+	 */
+
+	public function roots_support() {
+		add_theme_support( 'soil-clean-up' );
+		add_theme_support( 'soil-disable-asset-versioning' );
+		add_theme_support( 'soil-disable-trackbacks' );
+		// add_theme_support( 'soil-google-analytics', 'UA-XXXXXXXX-Y' );
+		add_theme_support( 'soil-jquery-cdn' );
+		// add_theme_support( 'soil-js-to-footer' );
+		add_theme_support( 'soil-nav-walker' );
+		add_theme_support( 'soil-nice-search' );
+		add_theme_support( 'soil-relative-urls' );
+	}
+
+	/**
+	 * Add typekit support if needed. Replace URL with one from the kit you create.
+	 */
+	public function typekit() {
+		wp_enqueue_script( 'theme_typekit', 'https://use.typekit.net/xbk1ivk.js' );
+		wp_inline_script( 'try{Typekit.load({ async: true });}catch(e){}' );
 	}
 }
-add_action( 'wp_head', 'theme_typekit_inline' );
+
+new ChildTheme();
--- gulpfile.js
+++ gulpfile.js
@@ -51,13 +51,11 @@ var project 	 = 'projectname', // Project name, used for build zip.
 		rename       = require('gulp-rename'),
 		concat       = require('gulp-concat'),
 		notify       = require('gulp-notify'),
-		cmq          = require('gulp-combine-media-queries'),
 		runSequence  = require('gulp-run-sequence'),
 		sass         = require('gulp-sass'),
 		plugins      = require('gulp-load-plugins')({ camelize: true }),
 		ignore       = require('gulp-ignore'), // Helps with ignoring files and directories in our run tasks
 		rimraf       = require('gulp-rimraf'), // Helps with removing files and directories in our run tasks
-		zip          = require('gulp-zip'), // Using to zip up our packaged theme into a tasty zip file that can be installed in WordPress!
 		plumber      = require('gulp-plumber'), // Helps prevent stream crashing on errors
 		cache        = require('gulp-cache'),
 		sourcemaps   = require('gulp-sourcemaps'),
@@ -86,7 +84,7 @@ gulp.task('browser-sync', function() {
 		// tunnel: true,
 
 		// Attempt to use the URL "http://my-private-site.localtunnel.me"
-		// tunnel: "kaseyacontentrepo",
+		// tunnel: "my-private-site",
 
 		// Inject CSS changes
 		injectChanges: true
@@ -123,7 +121,6 @@ gulp.task('styles', function () {
 				.pipe(plumber.stop())
 				.pipe(gulp.dest('./'))
 				.pipe(filter('**/*.css')) // Filtering stream to only css files
-				.pipe(cmq()) // Combines Media Queries
 				.pipe(reload({stream:true})) // Inject Styles when style file is created
 				.pipe(notify({ message: 'Styles task complete', onLast: true }));
 });
@@ -209,65 +206,6 @@ gulp.task('svgmin', function() {
    cache.clearAll();
  });
 
-
- /**
-  * Clean tasks for zip
-  *
-  * Being a little overzealous, but we're cleaning out the build folder, codekit-cache directory and annoying DS_Store files and Also
-  * clearing out unoptimized image files in zip as those will have been moved and optimized
- */
-
- gulp.task('cleanup', function() {
- 	return 	gulp.src(['./assets/bower_components', '**/.sass-cache','**/.DS_Store'], { read: false }) // much faster
-		 		.pipe(ignore('node_modules/**')) //Example of a directory to ignore
-		 		.pipe(rimraf({ force: true }));
-		 		// .pipe(notify({ message: 'Clean task complete', onLast: true }));
- });
- gulp.task('cleanupFinal', function() {
- 	return 	gulp.src(['./assets/bower_components','**/.sass-cache','**/.DS_Store'], { read: false }) // much faster
-		 		.pipe(ignore('node_modules/**')) //Example of a directory to ignore
-		 		.pipe(rimraf({ force: true }));
-		 		// .pipe(notify({ message: 'Clean task complete', onLast: true }));
- });
-
- /**
-  * Build task that moves essential theme files for production-ready sites
-  *
-  * buildFiles copies all the files in buildInclude to build folder - check variable values at the top
-  * buildImages copies all the images from img folder in assets while ignoring images inside raw folder if any
-  */
-
-  gulp.task('buildFiles', function() {
-  	return 	gulp.src(buildInclude)
- 		 		.pipe(gulp.dest(build))
- 		 		.pipe(notify({ message: 'Copy from buildFiles complete', onLast: true }));
-  });
-
-
-/**
-* Images
-*
-* Look at src/images, optimize the images and send them to the appropriate place
-*/
-gulp.task('buildImages', function() {
-	return 	gulp.src(['assets/img/**/*', '!assets/images/raw/**'])
-		 		.pipe(gulp.dest(build+'assets/img/'))
-		 		.pipe(plugins.notify({ message: 'Images copied to buildTheme folder', onLast: true }));
-});
-
- /**
-  * Zipping build directory for distribution
-  *
-  * Taking the build folder, which has been cleaned, containing optimized files and zipping it up to send out as an installable theme
- */
- gulp.task('buildZip', function () {
- 	// return 	gulp.src([build+'/**/', './.jshintrc','./.bowerrc','./.gitignore' ])
- 	return 	gulp.src(build+'/**/')
-		 		.pipe(zip(project+'.zip'))
-		 		.pipe(gulp.dest('./'))
-		 		.pipe(notify({ message: 'Zip task complete', onLast: true }));
- });
-
  // ==== TASKS ==== //
  /**
   * Gulp Default Task
@@ -275,12 +213,6 @@ gulp.task('buildImages', function() {
   * Compiles styles, fires-up browser sync, watches js and php files. Note browser sync task watches php files
   *
  */
-
- // Package Distributable Theme
- gulp.task('build', function(cb) {
- 	runSequence('styles', 'cleanup', 'vendorsJs', 'scriptsJs',  'buildFiles', 'buildImages', 'buildZip','cleanupFinal', cb);
- });
-
 
  // Watch Task
  gulp.task('default', ['styles', 'vendorsJs', 'scriptsJs', 'images', 'svgmin', 'browser-sync'], function () {
